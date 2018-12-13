d76f98c3d9b6036c4e148989c76a09b1b7dbfb98
x9sheikh <x9sheikh.usama@gmail.com>
2018-12-13 09:28:17 +0500

    added urls and register my app name

--- miniApp/smartNotes/urls.py
+++ miniApp/smartNotes/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('smartNotes/', include('smartNotes.urls')),
+    path('', views.index, name='index'),
 ]
