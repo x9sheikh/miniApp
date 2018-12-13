57f2d8f187be2cad53652477e3ea255499667e0d
x9sheikh <x9sheikh.usama@gmail.com>
2018-12-13 10:15:08 +0500

    added models

--- miniApp/smartNotes/admin.py
+++ miniApp/smartNotes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Subject, Mentor, SmartNotes
 # Register your models here.
+
+admin.site.register(SmartNotes)
+admin.site.register(Subject)
+admin.site.register(Mentor)
--- miniApp/smartNotes/models.py
+++ miniApp/smartNotes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Mentor(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class SmartNotes(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+    subjectName = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    mentorName = models.ForeignKey(Mentor, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
