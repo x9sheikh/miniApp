7a65dda4b5a070af8b81f1e8589f93dac02a958f
x9sheikh <x9sheikh.usama@gmail.com>
2018-12-17 22:44:14 +0500

    form completed successful

--- miniApp/smartNotes/forms.py
+++ miniApp/smartNotes/forms.py
@@ -3,13 +3,13 @@ from .models import MySmartNote
 
 class CreateForm(forms.Form):
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter your Title Here'}))
-    subject = forms.CharField(label="Subject Name")
-    mentor = forms.CharField(label="Mentor Name")
+    subject = forms.CharField(label="Subject Name", widget= forms.TextInput(attrs={'placeholder':'Enter your Subject Name'}))
+    mentor = forms.CharField(label="Mentor Name", widget=forms.TextInput(attrs={'placeholder': 'Enter your Mentor Name'}))
     context = forms.CharField(
         label="Context",
         widget= forms.Textarea(
             attrs={
-                'palceholder': 'Write your Notes Context, Here',
+                'placeholder': 'Write your Context, Here',
                 'rows': 10,
                 'cols': 80
             }
