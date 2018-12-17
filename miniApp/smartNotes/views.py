bece83e136a0b15633529dbb9ec3d0ebada6c884
x9sheikh <x9sheikh.usama@gmail.com>
2018-12-18 01:35:13 +0500

    Make edit form Successful

--- miniApp/smartNotes/views.py
+++ miniApp/smartNotes/views.py
@@ -11,27 +11,72 @@ def index(request):
     return render(request,'smartNotes/index.html', context)
 
 def detailNotes(request, notesId):
+
     class EditForm(forms.Form):
-        title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your Title Here'}))
-        subject = forms.CharField(label="Subject Name",
+        ######################## GET DATA FROM DATA BASE #########################
+
+        mySmartNotes = MySmartNote.objects.get(id=notesId)
+        myTitle = mySmartNotes.title
+        mySubject = mySmartNotes.subject
+        myMentor = mySmartNotes.mentor
+        myContext = mySmartNotes.context
+
+        ######################## HERE, IS A EDIT FORM ###########################
+        title = forms.CharField(initial=myTitle,
+                                widget=forms.TextInput(attrs={'placeholder': 'Enter your Title Here'}))
+        subject = forms.CharField(initial=mySubject,
+                                  label="Subject Name",
                                   widget=forms.TextInput(attrs={'placeholder': 'Enter your Subject Name'}))
-        mentor = forms.CharField(label="Mentor Name",
+        mentor = forms.CharField(initial=myMentor,
+                                 label="Mentor Name",
                                  widget=forms.TextInput(attrs={'placeholder': 'Enter your Mentor Name'}))
-        context = forms.CharField(
-            label="Context",
-            widget=forms.Textarea(
-                attrs={
-                    'placeholder': 'Write your Context, Here',
-                    'rows': 10,
-                    'cols': 80
-                }
-            ))
-    thisNote = MySmartNote.objects.get(id=notesId)
-    context = {
-        'title': thisNote.title,
-        'notesId': notesId
-    }
-    return render(request,'smartNotes/detailNotes.html', context)
+        context = forms.CharField(initial=myContext,
+                                  label="Context",
+                                  widget=forms.Textarea(
+                                      attrs={
+                                        'placeholder': 'Write your Context, Here',
+                                        'rows': 10,
+                                        'cols': 80
+                                            }
+                                                        )
+                                  )
+    form = EditForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            ##################### CREATE MY-SMART-NOTE OBJECT ################
+            mySmartNote = MySmartNote.objects.get(id=notesId)
+
+            ################## CREATE MENTOR OBJECT AND SET THE NEW NAME ##############
+            mentor = Mentor.objects.get(id=mySmartNote.mentor.id)
+            mentor.name = form.cleaned_data['mentor']
+            mentor.save()
+
+            ################## CREATE SUBJECT OBJECT AND SET THE NEW NAME ##############
+            subject = Subject.objects.get(id=mySmartNote.subject.id)
+            subject.name = form.cleaned_data['subject']
+            subject.save()
+
+            #################### SET TITLE AND CONTEXT TOO AND SAVE MY-SMART-NOTE#########################
+            mySmartNote.title = form.cleaned_data['title']
+            mySmartNote.context = form.cleaned_data['context']
+            mySmartNote.save()
+
+
+            return HttpResponse("Success")
+        else:
+            context = {
+                'notesId': notesId,
+                'error': 'Something going wrong'
+
+            }
+            return render(request, 'smartNotes/detailNotes.html', context)
+    else:
+        context = {
+            'form': form,
+            'notesId': notesId
+        }
+        return render(request, 'smartNotes/detailNotes.html', context)
+
 
 def createNotes(request):
     form = CreateForm(request.POST or None)
